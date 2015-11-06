b4537c668f6b94084c924819ae207675f5b6c32b
Henjo Hoeksma <henjo@upassist.com>
2015-11-06 13:04:48 +0100

    First working version

--- Plugin/Deployer.php
+++ Plugin/Deployer.php
@@ -4,6 +4,7 @@ namespace UpAssist\Deployer\Plugin;
 
 use PHPCI\Builder;
 use PHPCI\Model\Build;
+use UpAssist\Deployer\Utility\GitUtility;
 
 /**
  * Class Deployer
@@ -21,7 +22,7 @@ class Deployer implements \PHPCI\Plugin {
      */
     protected $build;
 
-    protected $configuration = [];
+    protected $options = [];
 
     protected $currentBranch;
 
@@ -36,28 +37,66 @@ class Deployer implements \PHPCI\Plugin {
         $this->phpci = $phpci;
         $this->build = $build;
 
-        if(isset($options['deployer'])) {
-            $this->configuration = $options['UpAssist\Deployer\Plugin\Deployer'];
+        if(!empty($options)) {
+            $this->options = $options;
         }
 
         $this->currentBranch = $this->build->getBranch();
+
     }
 
     public function execute()
     {
         // No configuration found
-        if (empty($this->configuration)) {
-            throw new \Exception('No deployer configuration found');
+        if (empty($this->options)) {
+            throw new \Exception('No deployer configuration found, are you sure you added configuration?');
         }
 
         /** @var array $branchConfiguration */
         $branchConfiguration = $this->getConfigurationForCurrentBranch();
 
-        // Current Branch has no configuration, silently ignore
-        if (empty($branchConfiguration)) {
-            return NULL;
+        // Current Branch does not exist in the configuration, silently ignore
+        if ($branchConfiguration === null) {
+            return null;
         }
 
+        // Copy the deploy.php if set
+        if (isset($this->options['deployFile'])) {
+            if($this->phpci->executeCommand('cp ' . $this->options['deployFile'] . ' ' . $this->build->currentBuildPath . '/deploy.php')) {
+                $this->phpci->log('Copied the deploy file successfully.');
+            }
+        }
+
+        // Validate the yaml configuration and if correct: deploy
+        if ($this->validateDeployerOptions($branchConfiguration)) {
+            // Lets deploy...
+            $env  = 'STAGE=' . $branchConfiguration['stage'] . ' ';
+            $env .= 'SERVER=' . $branchConfiguration['server'] . ' ';
+            $env .= 'USER=' . $branchConfiguration['user'] . ' ';
+            $env .= 'DEPLOY_PATH=' . $branchConfiguration['deploy_path'] . ' ';
+            $env .= 'BRANCH=' . $branchConfiguration['branch'] . ' ';
+            $env .= 'REPOSITORY=' . $branchConfiguration['repository'] . ' ';
+            $env .= 'SHARED_DIRS=' . implode(',', $branchConfiguration['shared_dirs']) . ' ';
+            $env .= 'WRITABLE_DIRS=' . implode(',', $branchConfiguration['writable_dirs']) . ' ';
+            $env .= 'BUILD=' . $this->build->getId() . ' ';
+            try {
+                $this->phpci->executeCommand('cd ' . $this->build->currentBuildPath);
+                $command = $env . 'dep deploy ' . $branchConfiguration['stage'];
+                if ($this->phpci->executeCommand($command)) {
+                    $success = true;
+                } else {
+                    $this->phpci->logFailure('Something went wrong at the deployer part.' . PHP_EOL . 'The following command was used:' . PHP_EOL . $command);
+                    $success = false;
+                }
+            } catch (\Exception $e) {
+                $this->phpci->logFailure('Something went wrong at the deployer part.', $e);
+                $success = false;
+            }
+        } else {
+            $success = false;
+        }
+
+        return $success;
     }
 
 
@@ -66,11 +105,26 @@ class Deployer implements \PHPCI\Plugin {
      */
     private function getConfigurationForCurrentBranch()
     {
-        return $this->configuration[$this->currentBranch];
+        if (isset($this->options[$this->currentBranch])) {
+            $branchConfiguration = $this->options[$this->currentBranch];
+
+            // setup the Git Repo to use for deployment
+            $gitUtility = new GitUtility();
+            $branchConfiguration['repository'] = $gitUtility->getCurrentRepositoryUri($this->phpci->buildPath);
+
+            // Set some defaults
+            $branchConfiguration['branch'] = $this->currentBranch;
+            // If stage is not set, we fall back to the branch name
+            $branchConfiguration['stage'] = isset($branchConfiguration['stage']) ? $branchConfiguration['stage'] : $this->currentBranch;
+            return $branchConfiguration;
+        }
+
+        return null;
     }
 
     /**
      * @param array $configuration
+     * @return bool
      * @throws \Exception
      */
     private function validateDeployerOptions($configuration = [])
@@ -79,8 +133,46 @@ class Deployer implements \PHPCI\Plugin {
             $configuration = $this->getConfigurationForCurrentBranch();
         }
 
-        if (empty($configuration['user'])) {
-            throw new \Exception('Deployer needs the user variable set');
+        if (empty($configuration['repository'])) {
+            $this->phpci->logFailure('Deployer needs the repository variable set, this should be done automatically');
+            return false;
         }
+
+        if (empty($configuration['user'])) {
+            $this->phpci->logFailure('Deployer needs the user variable set');
+            return false;
+        }
+
+        if (empty($configuration['server'])) {
+            $this->phpci->logFailure('Deployer needs the server variable set');
+            return false;
+        }
+
+        if (empty($configuration['deploy_path'])) {
+            $this->phpci->logFailure('Deployer needs the deploy_path variable set');
+            return false;
+        }
+
+        if (isset($configuration['shared_dirs']) && !is_array($configuration['shared_dirs'])) {
+            $this->phpci->logFailure('`shared_dirs` variable must be an array...');
+            return false;
+        }
+
+        if (isset($configuration['writable_dirs']) && !is_array($configuration['writable_dirs'])) {
+            $this->phpci->logFailure('`writable_dirs` variable must be an array...');
+            return false;
+        }
+
+        $output = '';
+        if (isset($this->options['deployFile'])) {
+            $output .= '<b>Deploy file:</b> ' . $this->options['deployFile'] . PHP_EOL;
+        }
+        foreach ($configuration as $key => $value) {
+            $output .= '<b>' . $key . ':</b> ';
+            $output .= is_array($value) ? implode(PHP_EOL, $value) . PHP_EOL : $value . PHP_EOL;
+        }
+        $this->phpci->log('Using the following configuration:' . PHP_EOL . $output);
+
+        return true;
     }
 }
--- Utility/GitUtility.php
+++ Utility/GitUtility.php
@@ -12,31 +12,11 @@ class GitUtility
      * @param string $directory
      * @return string
      */
-    public function getActiveBranch($directory = __DIR__)
-    {
-        exec("cd '$directory'; git branch", $lines);
-
-        $branch = '';
-        foreach ($lines as $line) {
-            if (strpos($line, '*') === 0) {
-                $branch = ltrim($line, '* ');
-                break;
-            }
-        }
-
-        return $branch;
-    }
-
-
-    /**
-     * @param string $directory
-     * @return string
-     */
-    public function getRepositoryUri($directory = __DIR__)
+    public function getCurrentRepositoryUri($directory = __DIR__)
     {
         exec("cd '$directory'; git remote -v", $lines);
         foreach ($lines as $line) {
-            $gitInfo = explode(' ', $line);
+            $gitInfo = explode(' ', preg_replace('/\s+/', ' ', $line));
             if ($gitInfo[0] === 'origin' && $gitInfo[2] === '(fetch)') {
                 return $gitInfo[1];
             }
